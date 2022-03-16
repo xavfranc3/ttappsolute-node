547f6c154bbe0362f24a5827bd78fb3bd3b93abf
Xav <xavfranc3@gmail.com>
2022-03-16 11:18:42 +0100

    Config and Validation setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
